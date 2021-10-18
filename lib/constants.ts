f29dc60761d57f23b8efa4562aae38b9f76f42d3
manjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2021-10-18 14:20:22 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,17 +14,17 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'http://hubli-zumba-club.vercel.app/';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'hublizumbaclub';
+export const BRAND_NAME = 'HubliZumbaClub';
+export const SITE_NAME_MULTILINE = ['Hubli', 'Zumba'];
+export const SITE_NAME = 'Hubli Zumba Club';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'Hubli Zumba Club';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
+  'Hubli Zumba Club';
+export const DATE = 'October 27, 2021';
 export const SHORT_DATE = 'Oct 27 - 9:00am PST';
 export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
@@ -44,34 +44,34 @@ export const NAVIGATION = [
     name: 'Stage A',
     route: '/stage/a'
   },
-  {
-    name: 'Stage C',
-    route: '/stage/c'
-  },
-  {
-    name: 'Stage M',
-    route: '/stage/m'
-  },
-  {
-    name: 'Stage E',
-    route: '/stage/e'
-  },
-  {
-    name: 'Schedule',
-    route: '/schedule'
-  },
-  {
-    name: 'Speakers',
-    route: '/speakers'
-  },
-  {
-    name: 'Expo',
-    route: '/expo'
-  },
-  {
-    name: 'Jobs',
-    route: '/jobs'
-  }
+//   {
+//     name: 'Stage C',
+//     route: '/stage/c'
+//   },
+//   {
+//     name: 'Stage M',
+//     route: '/stage/m'
+//   },
+//   {
+//     name: 'Stage E',
+//     route: '/stage/e'
+//   },
+//   {
+//     name: 'Schedule',
+//     route: '/schedule'
+//   },
+//   {
+//     name: 'Speakers',
+//     route: '/speakers'
+//   },
+//   {
+//     name: 'Expo',
+//     route: '/expo'
+//   },
+//   {
+//     name: 'Jobs',
+//     route: '/jobs'
+//   }
 ];
 
 export type TicketGenerationState = 'default' | 'loading';
